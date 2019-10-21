f511655ac969ee46e95766308b8f4bcb32c78239
Renato Rebouças <renato190698@gmail.com>
2019-10-21 20:05:57 -0300

    first commit

---
+++ all.py
@@ -0,0 +1,33 @@
+# raio = float(input("Digite o raio: "))
+# pi = 3.14159
+
+# print("A= " %pi*(raio)**2)
+
+## 1001  ##
+# a = int(input())
+# b = int(input())
+# x = a+b
+# print('X = %i' %x )
+
+##########
+
+## 1002 ##
+
+# r = float(input())
+# pi = float(3.14159)
+# area = (pi*(r**2))
+# print("A=%.4f" %area) 
+
+###########
+
+##  1003 ##
+a = int(input())
+b = int(input())
+soma = a+b
+print("SOMA = %i" %soma)
+
+############
+
+## 1004 ##
+
+
